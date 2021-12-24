fcf65a88d1f65943014242ba5e234e0ff3a4b063
ninoxin <ninoxin2003@mail.ru>
2021-12-24 11:22:05 +0300

    lesson03: не полностью только 4 вопроса

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 let title = "JavaScript";
 console.log(typeof title);
 
+const answ = prompt('Как называется ваш проект?', title);
+console.log(answ);
+
 let screens = "Простые, Сложные, Интерактивные";
 console.log(screens.length);
-screens = ['Простые, Сложные, Интерактивные'];
-console.log(screens);
+
+const answ2 = prompt('Какие типы экранов нужно разработать ?', screens);
+console.log(answ2);
+
 
 let screenPrice = "1233";
 console.log(screenPrice);
+
+const answ3 = prompt('Сколько будет стоить данная работа?', screenPrice);
+console.log(answ3);
+
+
 
 let rollback = Math.floor(Math.random() * 100);
 console.log(rollback);
@@ -18,7 +28,15 @@ console.log(typeof fullPrice);
 let adaptive = true;
 console.log(typeof adaptive);
 
+const answ4 = confirm('Нужен ли адаптив на сайте?');
+console.log(answ4);
+
 console.log('Стоимость верстки экранов ', screenPrice, '  рублей/ долларов/ гривен/ юани' + 'и Стоимость разработки сайта ', fullPrice, ' рублей/ долларов/гривен/юани');
 
 let result = 'fullPrice' * ('rollback' / 100);
 console.log(result);
+
+
+
+
+
