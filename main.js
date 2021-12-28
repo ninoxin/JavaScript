6efdf1efb67908cb9b6fe4d8d0681d5c96989333
ninoxin <ninoxin2003@mail.ru>
2021-12-28 09:08:01 +0300

    3урок

--- main.js
+++ main.js
@@ -1,42 +1,11 @@
-let title = "JavaScript";
-console.log(typeof title);
-
-const answ = prompt('Как называется ваш проект?', title);
-console.log(answ);
-
-let screens = "Простые, Сложные, Интерактивные";
-console.log(screens.length);
-
-const answ2 = prompt('Какие типы экранов нужно разработать ?', screens);
-console.log(answ2);
-
-
-let screenPrice = "1233";
-console.log(screenPrice);
-
-const answ3 = prompt('Сколько будет стоить данная работа?', screenPrice);
-console.log(answ3);
-
-
-
-let rollback = Math.floor(Math.random() * 100);
-console.log(rollback);
-
-let fullPrice = 1343;
-console.log(typeof fullPrice);
-
-let adaptive = true;
-console.log(typeof adaptive);
-
-const answ4 = confirm('Нужен ли адаптив на сайте?');
-console.log(answ4);
-
-console.log('Стоимость верстки экранов ', screenPrice, '  рублей/ долларов/ гривен/ юани' + 'и Стоимость разработки сайта ', fullPrice, ' рублей/ долларов/гривен/юани');
-
-let result = 'fullPrice' * ('rollback' / 100);
-console.log(result);
-
-
-
-
-
+let title = prompt('Как называется ваш проект?');
+let screens = prompt('Какие типы экранов нужно разработать ?', 'Простые, Сложные, Интерактивные')
+let screenPrice = +prompt('Сколько будет стоить данная работа?')
+let adaptive  = confirm('Нужен ли адаптив на сайте?');
+let service1 = prompt("Какой дополнительный тип услуги нужен?")
+let servicePrice1 = +prompt('Сколько это будет стоить?')
+let service2 = prompt("Какой дополнительный тип услуги нужен?")
+let servicePrice2 = +prompt('Сколько это будет стоить?')
+let rollback = 10
+let fullPrice = screenPrice + servicePrice1 + servicePrice2
+let servicePercentPrice  = fullPrice - (fullPrice  * (rollback / 100 ))
